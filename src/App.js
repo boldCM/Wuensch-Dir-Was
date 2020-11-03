2b5c0c4beb2c92d808fc16d5433ff700327a4980
boldCM <carolin@cc-meier.de>
2020-11-03 12:20:43 +0100

    Create WishlistItem and ButtonAdd

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Button from './components/Button';
+import GlobalStyle from './GlobalStyle';
+import WishListItem from './components/WishListItem';
 
 function App() {
   return (
     <Router>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contact</Link>
-        </li>
-      </ul>
+      <GlobalStyle />
       <Switch>
-        <Route path="/contact">contact</Route>
-        <Route path="/">Home</Route>
+        <Route path="/">
+          <WishListItem title="Caro" />
+          <Button>+</Button>
+        </Route>
       </Switch>
     </Router>
   );
---
+++ src/components/WishListItem.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import WishListItem from './WishListItem';
+
+export default {
+  title: 'WishlistItem',
+  component: WishListItem,
+};
+
+export const Standard = () => <WishListItem title="Caro" />;
