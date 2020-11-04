8d953dfa3e1f155a78bba5807bed3e713c59c1d3
boldCM <carolin@cc-meier.de>
2020-11-04 18:24:42 +0100

    Working links

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,17 +1,27 @@
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import styled from 'styled-components/macro';
 // import PropTypes from 'prop-types';
+import React, { useEffect, useState } from 'react';
+import { getListById } from '../api/lists';
 
 const DetailsContainer = styled.div`
   border: 1px solid darkblue;
 `;
 
 export default function Details() {
-  const { details } = useParams();
+  const { listId } = useParams();
+  const [list, setList] = useState(null);
 
+  useEffect(async () => {
+    const newList = await getListById(listId);
+    setList(newList);
+  }, []);
   return (
     <>
-      <DetailsContainer> Write some Wishes for {details}</DetailsContainer>
+      <DetailsContainer>
+        Write some Wishes for
+        <Link to={`/${listId}`}> {list?.title}</Link>
+      </DetailsContainer>
     </>
   );
 }
