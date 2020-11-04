6eeffed587f8baf68afe3039f1d093af4b7a4c9c
boldCM <carolin@cc-meier.de>
2020-11-04 15:16:25 +0100

    Import Local Api

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,7 +1,10 @@
 import styled from 'styled-components/macro';
 import PropTypes from 'prop-types';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import Button from '../components/Button';
 import WishListItem from '../components/WishListItem';
+import { getLists } from '../api/lists';
 
 const ContainerWelcome = styled.div`
   border: 1px solid black;
@@ -10,9 +13,24 @@ const ContainerWelcome = styled.div`
 
 // theoretisch wurde hier title für die WishListItem übergeben
 export default function Welcome({ goToDetailsLink, goToAddLink }) {
+  const [lists, setLists] = useState(null);
+
+  useEffect(async () => {
+    const newLists = await getLists();
+    setLists(newLists);
+  }, []);
+
   return (
     <>
       <ContainerWelcome>
+        <Link to="/philipp">Philipp&apos;s Wishlist</Link>
+        <Link to="/leon">Leon&apos;s Wishlist</Link>
+        {lists?.map((list) => (
+          <Link key={list.id} to={`/${list.id}`}>
+            {list.title}
+          </Link>
+        ))}
+
         <WishListItem goToDetailsLink={goToDetailsLink} />
         <Button>{goToAddLink}</Button>
       </ContainerWelcome>
