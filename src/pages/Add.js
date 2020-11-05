e846dc2279207a06d0eabb08933fc88e40db542d
boldCM <carolin@cc-meier.de>
2020-11-05 14:29:36 +0100

    Create Form Element on Add Page

--- src/pages/Add.js
+++ src/pages/Add.js
@@ -1,4 +1,6 @@
+import { useState } from 'react';
 import styled from 'styled-components/macro';
+import Button from '../components/Button';
 // import PropTypes from 'prop-types';
 
 const AddContainer = styled.div`
@@ -6,9 +8,29 @@ const AddContainer = styled.div`
 `;
 
 export default function AddPage() {
+  const [value, setValue] = useState('');
+
+  const onChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // eslint-disable-next-line no-alert
+    alert(`submitted ${value}`);
+  };
+
   return (
     <>
-      <AddContainer>Add a new List</AddContainer>
+      <AddContainer>
+        Add a new List for
+        <form onSubmit={handleSubmit}>
+          <label placeholder="Enter Name">
+            <input type="text" value={value} onChange={onChange} />
+          </label>
+          <Button type="submit">Add</Button>
+        </form>
+      </AddContainer>
     </>
   );
 }
